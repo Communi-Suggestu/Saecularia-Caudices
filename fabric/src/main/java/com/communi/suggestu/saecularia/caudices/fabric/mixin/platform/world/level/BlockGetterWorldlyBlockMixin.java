79a5bfdcf2a9eb8de267b52eca6243c37ca431c9
Marc Hermans <marc.hermans@ldtteam.com>
2022-11-14 10:19:02 +0100

    Add support light level detection in BlockGetter.

---
+++ fabric/src/main/java/com/communi/suggestu/saecularia/caudices/fabric/mixin/platform/world/level/BlockGetterWorldlyBlockMixin.java
@@ -0,0 +1,25 @@
+package com.communi.suggestu.saecularia.caudices.fabric.mixin.platform.world.level;
+
+import com.communi.suggestu.saecularia.caudices.core.block.IBlockWithWorldlyProperties;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BlockGetter.class)
+public abstract class BlockGetterWorldlyBlockMixin {
+
+    @Shadow public abstract BlockState getBlockState(BlockPos pos);
+
+    @Inject(method = "getLightEmission(Lnet/minecraft/core/BlockPos;)I", at = @At("HEAD"), cancellable = true)
+    public void onGetLightEmission(final BlockPos pos, final CallbackInfoReturnable<Integer> cir) {
+        final BlockState blockState = getBlockState(pos);
+        if (blockState.getBlock() instanceof IBlockWithWorldlyProperties blockWithWorldlyProperties) {
+            cir.setReturnValue(blockWithWorldlyProperties.getLightEmission(blockState, (BlockGetter) this, pos));
+        }
+    }
+}
