af59c782605b707ae6c23a2989d8e302cd433dfc
Marc Hermans <marc.hermans@ldtteam.com>
2022-11-14 10:37:34 +0100

    Trying this with an interface mixin.

--- fabric/src/main/java/com/communi/suggestu/saecularia/caudices/fabric/mixin/platform/world/level/BlockGetterWorldlyBlockMixin.java
+++ fabric/src/main/java/com/communi/suggestu/saecularia/caudices/fabric/mixin/platform/world/level/BlockGetterWorldlyBlockMixin.java
@@ -11,12 +11,12 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BlockGetter.class)
-public abstract class BlockGetterWorldlyBlockMixin {
+public interface BlockGetterWorldlyBlockMixin {
 
     @Shadow public abstract BlockState getBlockState(BlockPos pos);
 
     @Inject(method = "getLightEmission(Lnet/minecraft/core/BlockPos;)I", at = @At("HEAD"), cancellable = true)
-    public void onGetLightEmission(final BlockPos pos, final CallbackInfoReturnable<Integer> cir) {
+    public default void onGetLightEmission(final BlockPos pos, final CallbackInfoReturnable<Integer> cir) {
         final BlockState blockState = getBlockState(pos);
         if (blockState.getBlock() instanceof IBlockWithWorldlyProperties blockWithWorldlyProperties) {
             cir.setReturnValue(blockWithWorldlyProperties.getLightEmission(blockState, (BlockGetter) this, pos));
