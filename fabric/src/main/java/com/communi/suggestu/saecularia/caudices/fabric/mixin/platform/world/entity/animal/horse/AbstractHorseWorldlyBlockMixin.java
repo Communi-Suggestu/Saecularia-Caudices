e2dfdd9f68253fa1694995e852794060578e9afe
Marc Hermans <marc.hermans@ldtteam.com>
2025-11-25 21:59:33 +0100

    Build for 1.21.11-pre2

--- fabric/src/main/java/com/communi/suggestu/saecularia/caudices/fabric/mixin/platform/world/entity/animal/horse/AbstractHorseWorldlyBlockMixin.java
+++ fabric/src/main/java/com/communi/suggestu/saecularia/caudices/fabric/mixin/platform/world/entity/animal/horse/AbstractHorseWorldlyBlockMixin.java
@@ -5,7 +5,7 @@ import com.communi.suggestu.saecularia.caudices.fabric.mixin.platform.world.enti
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.animal.horse.AbstractHorse;
+import net.minecraft.world.entity.animal.equine.AbstractHorse;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
--- fabric/src/main/java/com/communi/suggestu/saecularia/caudices/fabric/mixin/platform/world/entity/vehicle/BoatWorldlyBlockMixin.java
+++ fabric/src/main/java/com/communi/suggestu/saecularia/caudices/fabric/mixin/platform/world/entity/vehicle/BoatWorldlyBlockMixin.java
@@ -2,23 +2,21 @@ package com.communi.suggestu.saecularia.caudices.fabric.mixin.platform.world.ent
 
 import com.communi.suggestu.saecularia.caudices.core.block.IBlockWithWorldlyProperties;
 import com.communi.suggestu.saecularia.caudices.fabric.mixin.platform.world.entity.EntityAccessor;
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.vehicle.AbstractBoat;
-import net.minecraft.world.entity.vehicle.Boat;
+import net.minecraft.world.entity.vehicle.boat.AbstractBoat;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.AbstractBannerBlock;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.shapes.VoxelShape;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(value = AbstractBoat.class, priority = Integer.MIN_VALUE)
 public abstract class BoatWorldlyBlockMixin extends Entity
@@ -33,25 +31,22 @@ public abstract class BoatWorldlyBlockMixin extends Entity
 
 
     @Inject(
-            method = "getGroundFriction",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/core/BlockPos$MutableBlockPos;set(III)Lnet/minecraft/core/BlockPos$MutableBlockPos;"
-            ),
-            locals = LocalCapture.CAPTURE_FAILHARD
+        method = "getGroundFriction",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/core/BlockPos$MutableBlockPos;set(III)Lnet/minecraft/core/BlockPos$MutableBlockPos;"
+        )
     )
-    private void injectGetFrictionAdaptorForPosition(final CallbackInfoReturnable<Float> cir, final AABB aABB, final AABB aABB2, final int i, final int j, final int k, final int l, final int m, final int n, final VoxelShape voxelShape, final float f, final int o, final BlockPos.MutableBlockPos mutableBlockPos)
+    private void injectGetFrictionAdaptorForPosition(final CallbackInfoReturnable<Float> cir, @Local final BlockPos.MutableBlockPos mutableBlockPos)
     {
         this.workingPos = mutableBlockPos;
     }
 
-    @SuppressWarnings("InvalidInjectorMethodSignature")
+    @Definition(id = "getBlockState", method = "Lnet/minecraft/world/level/Level;getBlockState(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;")
+    @Expression("? = ?.getBlockState(?)")
     @ModifyVariable(
             method = "getGroundFriction",
-            at = @At(
-                    value = "INVOKE_ASSIGN",
-                    target = "Lnet/minecraft/world/level/Level;getBlockState(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;"
-            ),
+            at = @At(value = "MIXINEXTRAS:EXPRESSION", shift = At.Shift.AFTER),
             ordinal = 0
     )
     private BlockState injectGetFrictionAdaptorForState(final BlockState current)
